af09074f769eacf58f3292162145048f031b0dbf
kanokwan-st <ganokwn123@gmail.com>
2023-10-19 18:20:00 +0700

    feature get auth user

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -61,3 +61,7 @@ exports.login = async (req, res, next) => {
     next(err);
   }
 };
+
+exports.getMe = (req, res, next) => {
+  res.status(200).json({ user: req.user }); //ส่งข้อมูล user กลับไป
+}
--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -2,5 +2,13 @@ const chalk = require("chalk");
 
 module.exports = (err, req, res, next) => {
   console.log(chalk.redBright.bold.italic(err));
-  res.status(500).json({ message: err.message });
+
+  if (err.name === "ValidationError") {
+    err.statusCode = 400;
+  } else if (err.name === "TokenExpiredError") {
+    err.statusCode = 401;
+  } else if (err.name === "JsonWebTokenError") {
+    err.statusCode = 401;
+  }
+  res.status(err.statusCode || 500).json({ message: err.message });
 };
