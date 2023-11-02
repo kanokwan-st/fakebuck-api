d6186a70798735d9ca2bb4d31d6ce65493e7a95b
kanokwan-st <ganokwn123@gmail.com>
2023-11-02 22:14:53 +0700

    create, get all, delete post

--- src/routes/post-route.js
+++ src/routes/post-route.js
@@ -6,6 +6,7 @@ const upload = require('../middlewares/upload');
 const router = express.Router();
 
 router.post('/', upload.single('postImage'), postController.createPost);
+router.get('/friends', postController.getAllPostIncludeFriend);
 
 module.exports = router;
 
